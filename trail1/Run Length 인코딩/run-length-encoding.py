b14b1740c39bbb9f9b085c2260668ceb11f540a3
yurin-bee <noturine@gmail.com>
2026-09-16 17:36:11 +0900

    [Passed] Run Length 인코딩 (48ms, 16MB)

--- trail1/Run Length 인코딩/run-length-encoding.py
+++ trail1/Run Length 인코딩/run-length-encoding.py
@@ -1,5 +1,21 @@
 A = input()
 
-# Please write your code here.
-A.sort()
-print(A)
+result = []
+cur = A[0]
+cnt = 1
+
+for i in A[1:]:
+    if i == cur:
+        cnt += 1
+    else:
+        result.append(cur)
+        result.append(cnt)
+        cur = i
+        cnt = 1
+
+result.append(cur)
+result.append(cnt)
+
+encoded = ''.join(str(x) for x in result)
+print(len(encoded))
+print(encoded)
