63c21a831640e1da7eaf2bc113227403840ba361
yurin-bee <noturine@gmail.com>
2026-09-16 17:52:12 +0900

    [Passed] Run Length 인코딩 (57ms, 16MB)

--- trail1/Run Length 인코딩/run-length-encoding.py
+++ trail1/Run Length 인코딩/run-length-encoding.py
@@ -1,21 +1,7 @@
+from itertools import groupby
+
 A = input()
 
-result = []
-cur = A[0]
-cnt = 1
-
-for i in A[1:]:
-    if i == cur:
-        cnt += 1
-    else:
-        result.append(cur)
-        result.append(cnt)
-        cur = i
-        cnt = 1
-
-result.append(cur)
-result.append(cnt)
-
-encoded = ''.join(str(x) for x in result)
-print(len(encoded))
-print(encoded)
+encodded = ''.join(f"{char}{len(list(lst))}" for char, lst in groupby(A))
+print(len(encodded))
+print(encodded)
