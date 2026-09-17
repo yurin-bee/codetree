1123c56be35e3da35fab40aace987681fd3ae539
yurin-bee <noturine@gmail.com>
2026-09-17 10:25:57 +0900

    [Passed] 부분문자열 위치 구하기 (50ms, 16MB)

--- trail1/부분문자열 위치 구하기/find-location-of-substring.py
+++ trail1/부분문자열 위치 구하기/find-location-of-substring.py
@@ -2,4 +2,12 @@ input_str = input()
 target_str = input()
 
 # Please write your code here.
-print(input_str.find(target_str))
+inp = len(input_str)
+t = len(target_str)
+
+for i in range(inp-t+1):
+    if input_str[i:i+t] == target_str:
+        print(i)
+        break
+else:
+    print(-1)
