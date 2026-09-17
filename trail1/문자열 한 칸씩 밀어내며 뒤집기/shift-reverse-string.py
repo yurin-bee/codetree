3c8329aa9de8eca2acd4da2187c556dcb9d853f4
yurin-bee <noturine@gmail.com>
2026-09-17 12:15:29 +0900

    [Passed] 문자열 한 칸씩 밀어내며 뒤집기 (48ms, 16MB)

--- trail1/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
+++ trail1/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
@@ -4,7 +4,7 @@ for _ in range(q):
     quest = int(input())
     if quest == 1:
         s = s[1:] + s[0]
-    if quest == 2:
+    elif quest == 2:
         s = s[-1] + s[0:-1]
     else:
         s = s[::-1]
