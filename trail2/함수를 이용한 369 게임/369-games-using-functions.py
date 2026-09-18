1fe3dff37dd6b515cf9f673e54cd2039be913802
yurin-bee <noturine@gmail.com>
2026-09-18 15:52:57 +0900

    [Passed] 함수를 이용한 369 게임 (141ms, 19MB)

--- trail2/함수를 이용한 369 게임/369-games-using-functions.py
+++ trail2/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,12 +1,16 @@
-a, b = map(int, input().split())
+def has_369(n):
+    return any(d in str(n) for d in '369')
 
-def function(a, b):
+def is_multiple_of_3(n):
+    return n % 3 == 0
+
+def count_369_game(a, b):
     cnt = 0
-    for i in range(a, b+1):
-        s = str(i)
-        if '3' in s or '6' in s or '9' in s:
-            if i % 3 == 0:
-                cnt += 1
+    for i in range(a, b + 1):
+        if has_369(i) or is_multiple_of_3(i):
+            cnt += 1
     return cnt
 
-print(function(a, b))
+
+a, b = map(int, input().split())
+print(count_369_game(a, b))
