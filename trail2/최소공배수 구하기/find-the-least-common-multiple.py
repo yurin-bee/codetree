e1201e72b1dee14cb8d227b752e2a4a21d5528bf
yurin-bee <noturine@gmail.com>
2026-09-18 15:08:37 +0900

    [Wrong Answer] 최소공배수 구하기 (53ms, 16MB)

--- trail2/최소공배수 구하기/find-the-least-common-multiple.py
+++ trail2/최소공배수 구하기/find-the-least-common-multiple.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 
 # Please write your code here.
 def choi(n, m):
-    for i in range(1, 101):
+    for i in range(m, 101):
         if i % n == 0 and i % m ==0:
             print(i)
             break
