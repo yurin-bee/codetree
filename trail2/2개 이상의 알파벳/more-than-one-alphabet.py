d30580b9253ba028ed6464a22e2f015107895ca3
yurin-bee <noturine@gmail.com>
2026-09-21 15:06:10 +0900

    [Wrong Answer] 2개 이상의 알파벳 (61ms, 17MB)

--- trail2/2개 이상의 알파벳/more-than-one-alphabet.py
+++ trail2/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -2,6 +2,14 @@ A = input()
 
 # Please write your code here.
 
-from itertools import groupby
-
-print(list(A).groupby())
+def alphabet(A):
+    A = list(A)
+    for i in range(len(A)):
+        cnt = 0
+        for j in A:
+            if i == j:
+                cnt += 1
+        if cnt >= 2:
+            return True
+    return False
+print(alphabet(A))
