a731fb55e13cbe60d983c63a915bad04138db679
yurin-bee <noturine@gmail.com>
2026-09-21 17:05:44 +0900

    [Passed] 나누고 빼면서 합하기 (49ms, 16MB)

--- trail2/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ trail2/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -1,10 +1,19 @@
-def reduce(n, m, A):
-    sum_value = 0
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+
+def reduce(n,m,A):
+    sum = 0
     while True:
-        sum_value += A[m-1]
-        if m == 1:              # ← 값 더하고 나서, m을 아직 안 건드린 상태에서 바로 체크
-            return sum_value
+        sum += A[m-1]
+        if m == 1:
+            return sum
         if m % 2 == 1:
             m -= 1
         else:
             m //= 2
+
+
+print(reduce(n,m,A))
+    
